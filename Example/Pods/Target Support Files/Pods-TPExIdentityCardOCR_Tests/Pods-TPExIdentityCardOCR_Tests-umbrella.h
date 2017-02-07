d1fa12a68785763d70686e23d3376b35ba73b85f
kingdomrain <bygd2014@sina.com>
2017-02-07 15:31:07 +0800

    移除项目
    移除项目

--- Example/Pods/Target Support Files/Pods-TPExIdentityCardOCR_Tests/Pods-TPExIdentityCardOCR_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_TPExIdentityCardOCR_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TPExIdentityCardOCR_TestsVersionString[];
-
--- Example/Pods/Target Support Files/TPExIdentityCardOCR/TPExIdentityCardOCR-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TPExIdentityCardOCR : NSObject
-@end
-@implementation PodsDummy_TPExIdentityCardOCR
-@end
