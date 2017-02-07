986aa5ed33a3837dc7245f9e32b895ce0804b4f1
kingdomrain <bygd2014@sina.com>
2017-02-07 20:45:23 +0800

    修改

--- Example/Pods/Target Support Files/TPExIdentityCardOCR/TPExIdentityCardOCR-umbrella.h
+++ Example/Pods/Target Support Files/TPExIdentityCardOCR/TPExIdentityCardOCR-umbrella.h
@@ -2,6 +2,7 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "TPExIdentityCardOCR.h"
 
 FOUNDATION_EXPORT double TPExIdentityCardOCRVersionNumber;
 FOUNDATION_EXPORT const unsigned char TPExIdentityCardOCRVersionString[];
